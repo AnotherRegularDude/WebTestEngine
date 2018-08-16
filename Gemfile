1fff793357111c951320bf8256e23774ac7d3885
Ivan Chernov <vanyaz158@gmail.com>
2018-08-16 20:26:22 +0300

    Change handling of settings.yml

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'awesome_print'
 gem 'bcrypt', '~> 3.1.7'
 gem 'draper'
 gem 'ffaker'
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module WebTestEngine
     config.available_locales = %i[en ru]
     config.default_locale = :en
 
-    config.x = Hashie::Mash.new YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'settings.yml'))).result)
+    config.x = Hashie::Mash.new(Rails.application.config_for(:settings))
 
     config.cache_store = :redis_store, "#{config.x.global_info.redis.connection_url}/cache", { expires_in: 90.minutes }
     config.session_store :cookie_store, expire_after: 14.days
